2602c9f0bd07bc1e83275a92198eab10bd2559a4
SandeepKhadka <87227387+SandeepKhadka@users.noreply.github.com>
2024-08-06 12:16:13 +0545

    js intro

---
+++ index.js
@@ -0,0 +1,90 @@
+// print in js terminal
+
+// console.log("First js")
+// console.log("Hello world")
+
+
+/*  
+    single-line comment = ctrl + / => //
+
+
+    multi--line comment = shift + alt + a => 
+
+*/
+
+/* 
+
+    How to print in different programming languages
+
+    in python => print("Hello world")
+    in php => echo "hello world"
+    java => System.out.println("Hello world")
+
+
+*/
+
+// Datatypes in js
+/* 
+console.log("This is a string") // This is a string datatype example
+
+console.log("Number in js : ", 100) // This is a number datatype example
+
+console.log("Married :" , true)
+
+age = 20
+firstName = "John Doe" // camel case
+console.log(firstName) // prints John Doe
+firstName = "John Cena"
+console.log(firstName) // prints John Doe
+
+first_name = "KTM"
+console.log(first_name) // snake case
+number = 200 */
+// console.log(name)
+
+// Local and Global Variables 
+
+// Local variables are block or functional scope
+// Global variables are in all over the file or program
+
+
+// let vs var vs const
+
+
+/* 
+    To declare variable/constants in js
+     <Keyword> <specific/meaningful name> = <value>
+
+*/
+
+// declare and initialize
+let age = 10;
+var last_name = "Sharma"
+const PI = 3.14
+
+// Declare
+
+let age1
+
+
+// Initialize
+
+age1 = 20
+
+// scope, redeclaration, hoisting
+
+const GRAVITY = 9.8
+
+let middle_name = "bahadur"
+middle_name = "ok"
+
+// function myfunction(){
+
+// }
+
+console.log(middle_name)
+
+
+// GRAVITY = 10
+
+console.log(GRAVITY)
