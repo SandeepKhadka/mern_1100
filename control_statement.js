200f2d959547a6f91ce121f2b81e24d1156779f7
SandeepKhadka <87227387+SandeepKhadka@users.noreply.github.com>
2024-08-15 11:51:30 +0545

    Control Statement

---
+++ control_statement.js
@@ -0,0 +1,85 @@
+/* 
+
+    a) if - else
+    b) else - if
+    c) switch - case
+
+    d) loop
+        - while
+        - do-while
+        - for
+        - for....of
+        - for....in
+    
+    break , continue
+
+*/
+
+let percentage = 37;
+
+// if syntax
+
+// if(exp){
+//     // if body
+// }else{
+//     // else body
+// }
+
+if (percentage >= 80) {
+  // if body
+  console.log("First Div with Distinction");
+} else {
+  if (percentage >= 60 && percentage < 80) {
+    console.log("First Div");
+  } else {
+    if (percentage >= 45 && percentage < 60) {
+      console.log("Second Div");
+    } else {
+      if (percentage >= 35 && percentage < 45) {
+        console.log("Third Div");
+      }
+    }
+  }
+  // else body
+}
+// console.log("Your marks is less than 80");
+
+// else if syntax
+
+if(percentage >= 80){
+    console.log("First Div with Distinction")
+} else if(percentage >= 60 && percentage < 80){
+    console.log("First Div");
+} else if(percentage >= 45 && percentage < 60){
+    console.log("Second Div");
+}else if(percentage >= 35 && percentage < 45){
+    console.log("Third Div");
+} else{
+    console.log("Failed");
+    
+}
+
+// switch case syntax
+
+let act = "show" // add, edit, show, delete, 
+
+switch(act){
+    case 'add':
+        console.log("This is add action")
+        break;
+    
+    case 'show':
+        console.log("This is show action");
+        break
+
+    case 'edit':
+        console.log("This is edit action");
+        break
+    
+    case 'delete':
+        console.log("This is delete action");
+        break
+    
+    default:
+        console.log("No match found")    
+}
--- operators.js
+++ operators.js
@@ -24,6 +24,22 @@
             let a = 10
             a > 10 ? "Print a is greater" : a > 5 ? "Print a is greater than 5 but less that 10" : "Print a is more smaller than 5"
 
+    Task - 
+        let percentage = 45;
+
+        >=80 => First Div with Distinction
+        >=60 && <80 => First Div
+        >=45 && <60 => Second Div
+        >=35 && <45 => Third Div
+        <35 => Sorry! You are failed.
+
+    Task Answer -
+
+        let percentage = 37;
+        percentage >=80 ? "First Div with Distinction" : (percentage >=60 && percentage <80) ? "First Div" : (percentage >=45 && percentage <60 ) ? "Second Div" : (percentage >=35 && percentage < 45) ? "Third Div" : "You are failed"
+
+
+
 */
 
 // output - ram sharma
