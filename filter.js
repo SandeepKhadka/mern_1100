c09cf1fc3da0710588a052d647dc1630b6a51e07
SandeepKhadka <87227387+SandeepKhadka@users.noreply.github.com>
2024-08-20 12:22:34 +0545

    filter

---
+++ filter.js
@@ -0,0 +1,8 @@
+let evenArray = [2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].filter((el) => {
+  if (el % 2 == 0) {
+    return true;
+  }
+  return false;
+});
+
+console.log(evenArray);
--- map.js
+++ map.js
@@ -1,3 +1,10 @@
+/* 
+    map function
+
+        returns new array of exact same length of old array
+
+*/
+
 let array = [2, 4, 5, 6]
 let doubleArray = [...array].map((el) => {
     return el * 3
@@ -11,12 +18,7 @@ let doubleArray = [...array].map((el) => {
 console.log(doubleArray);
 
 
-/* 
-    map function
 
-        returns new array of exact same length of old array
-
-*/
 
 // doubleArray
 
