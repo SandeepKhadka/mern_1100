a01b8dc019c6b5a797ce17910c02f90a2546e066
SandeepKhadka <87227387+SandeepKhadka@users.noreply.github.com>
2024-08-22 11:42:41 +0545

    callback

---
+++ promise.js
@@ -0,0 +1,34 @@
+function task1(callback) {
+  setTimeout(() => {
+    console.log("Task 1 is completed");
+    callback();
+  }, 2000);
+}
+function task2(callback) {
+  setTimeout(() => {
+    console.log("Task 2 is completed");
+    callback();
+  }, 1000);
+}
+function task3(callback) {
+  setTimeout(() => {
+    console.log("Task 3 is completed");
+    callback();
+  }, 4000);
+}
+function task4(callback) {
+  setTimeout(() => {
+    console.log("Task 4 is completed");
+    callback();
+  }, 3000);
+}
+
+task1(() => {
+  task2(() => {
+    task3(() => {
+      task4(() => console.log("All tasks completed"));
+    });
+  });
+});
+
+// console.log("All tasks completed");
